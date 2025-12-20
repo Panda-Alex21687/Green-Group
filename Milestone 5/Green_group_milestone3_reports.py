060471f7b9a0f0d49955def6393adb5de8816501
Panda-Alex21687 <Alex21687@gmail.com>
2025-12-20 00:17:11 -0600

    Added Original py and Revised to zip Milestone 5

---
+++ Milestone 5/Green_group_milestone3_reports.py
@@ -0,0 +1,144 @@
+# Green team Milestone 3 Python code 
+# Alexander Baldree
+# Jordan Dardar
+# Maksymilian Jankowski
+# Aftabur Rahman
+
+
+import mysql.connector
+
+# ----------------------------
+# CONNECT TO DATABASE
+# Making connection to outland database 
+# ----------------------------
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="jess1234",
+    database="outland"
+)
+
+cursor = db.cursor()
+
+print("\n==============================")
+print(" OUTLAND ADVENTURES REPORTS")
+print("==============================\n")
+
+# -------------------------------------------
+# REPORT 1: EQUIPMENT OLDER THAN 5 YEARS
+# REquired busness decision: replacing equipment after 5 years
+# Query to serch inventory and inform of equipment to replace
+# -------------------------------------------
+print("REPORT 1: Equipment Older Than 5 Years\n")
+
+query_equipment_age = """
+SELECT equipment_id, item_name, acquired_date,
+       TIMESTAMPDIFF(YEAR, acquired_date, CURDATE()) AS age_years
+FROM EQUIPMENT
+WHERE acquired_date <= DATE_SUB(CURDATE(), INTERVAL 5 YEAR);
+"""
+
+cursor.execute(query_equipment_age)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("\n-------------------------------------------\n")
+
+# -------------------------------------------
+# REPORT 2: BOOKINGS BY REGION
+# required business decision to identify which regions have declining interest 
+# shows total booking per region 
+# -------------------------------------------
+print("REPORT 2: Booking Trends by Region\n")
+
+query_region_trends = """
+SELECT r.region_name, COUNT(b.booking_id) AS total_bookings
+FROM REGION r
+LEFT JOIN TRIP t ON r.region_id = t.region_id
+LEFT JOIN BOOKING b ON t.trip_id = b.trip_id
+GROUP BY r.region_name
+ORDER BY total_bookings DESC;
+"""
+
+cursor.execute(query_region_trends)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("\n-------------------------------------------\n")
+
+# -------------------------------------------
+# REPORT 3: SALES VS RENTAL REVENUE
+# Third required business decision that checks equipment inventory based on sales vs rentals
+# -------------------------------------------
+print("REPORT 3: Sales vs Rental Revenue\n")
+
+query_sales_vs_rentals = """
+SELECT order_type,
+       SUM(ol.quantity * ol.price_each) AS total_revenue
+FROM EQUIPMENT_ORDER eo
+JOIN ORDER_LINE ol ON eo.order_id = ol.order_id
+GROUP BY order_type;
+"""
+
+cursor.execute(query_sales_vs_rentals)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("\n-------------------------------------------\n")
+
+# report 4 guide assignments 
+# Additional report to show guide workloads 
+# shows how many trips each guide takes 
+print("REPORT 4: Guide Assignment Report\n")
+
+query_guide_assignments = """
+SELECT e.first_name, e.last_name, e.role,
+       COUNT(t.trip_id) AS trips_assigned
+FROM EMPLOYEE e
+LEFT JOIN TRIP t ON e.employee_id = t.guide_id
+WHERE e.role = 'Guide'
+GROUP BY e.employee_id, e.first_name, e.last_name, e.role
+ORDER BY trips_assigned DESC;
+"""
+
+cursor.execute(query_guide_assignments)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("\n-------------------------------------------\n")
+
+# report 5 frequency of equipment order 
+# Additional report to identify popular equipmnent sales and rentals 
+# Identifies which equipment generates most revenue 
+print("REPORT 5: Equipment Order Frequency\n")
+
+query_equipment_popularity = """
+SELECT e.item_name, e.item_type,
+       COUNT(DISTINCT ol.order_id) AS times_ordered,
+       COALESCE(SUM(ol.quantity), 0) AS total_quantity_ordered,
+       COALESCE(SUM(ol.quantity * ol.price_each), 0) AS total_revenue
+FROM EQUIPMENT e
+LEFT JOIN ORDER_LINE ol ON e.equipment_id = ol.equipment_id
+GROUP BY e.equipment_id, e.item_name, e.item_type
+ORDER BY total_revenue DESC;
+"""
+
+cursor.execute(query_equipment_popularity)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+print("\n-------------------------------------------\n")
+
+
+
+print("All reports generated!")
